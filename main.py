7319fb263ace37026b04d70775cd439d16224f11
Bodya567384 <itkaterynchuk124@gmail.com>
2024-11-02 14:54:52 +0200

    Python_Own_Project 3

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+from crypt import methods
+from pyexpat.errors import messages
+from werkzeug.security import check_password_hash
 from flask import Flask, request, session, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 import os
+
+SESSION_USER_ID = 'user_id'
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(basedir, 'retro.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+app.config['SECRET KEY'] = 'wO68AEm3cSknDmKt2ofLvx2yJwN_vf9wxzhU3geUeZs'
 db = SQLAlchemy(app)
 
 class User(db.Model):
@@ -15,6 +20,17 @@ class User(db.Model):
     email = db.Column(db.String(150), nullable=False)
     password = db.Column(db.String(25), nullable=False)
     role = db.Column(db.Integer(), default=0, nullable=False)
+
+    def __repr__(self):
+        return f'<User: {self.username}>'
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+
+with app.app_context():
+    db.create_all()
+
 
 @app.route('/')
 def index():
@@ -27,6 +43,32 @@ def about():
 @app.route('/services')
 def services():
     return render_template('services.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    message = ''
+
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        user = User.query.filter_by(email=email).first()
+
+        if not user:
+            message = 'Invalid Email'
+        else:
+            if user.check_password(password):
+                session[SESSION_USER_ID] = user.id
+                return redirect('/')
+
+            message = 'Invalid Password'
+
+    return render_template('login.html', message=message)
+
+@app.route('/logout')
+def logout():
+    session.pop(SESSION_USER_ID, None)
+    return redirect('/')
 
 @app.route('/portfolio')
 def portfolio():
